1f80ee7986a23974e9e04b9a95ef2c47212e9e79
JonghyeonC <109335419+JonghyeonC@users.noreply.github.com>
2022-08-21 23:12:02 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 164 ms, Memory: 61,472 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,39 +1,33 @@
-num = int(input())
-for tc in range(1, num+1):
+T = int(input())
+for tc in range(1, T+1):
     n, k = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(n)]
-    cnt = 0
-    total = 0
 
+    answer = 0
     for i in range(n):
         cnt = 0
         for j in range(n):
             if arr[i][j] == 1:
                 cnt += 1
+            elif arr[i][j] == 0:
                 if cnt == k:
-                    if j == n - 1:
-                        total += 1
-                        cnt = 0
-                    else:
-                        if arr[i][j+1] != 1:
-                            total += 1
-                            cnt = 0
-            else:
-                cnt = 0
-
+                    answer += 1
+                    cnt = 0
+                else:
+                    cnt = 0
+        if cnt == k:
+            answer += 1
     for i in range(n):
         cnt = 0
         for j in range(n):
             if arr[j][i] == 1:
                 cnt += 1
+            elif arr[j][i] == 0:
                 if cnt == k:
-                    if j == n - 1:
-                        total += 1
-                        cnt = 0
-                    else:
-                        if arr[j+1][i] != 1:
-                            total += 1
-                            cnt = 0
-            else:
-                cnt = 0
-    print(f'#{tc} {total}')
+                    answer += 1
+                    cnt = 0
+                else:
+                    cnt = 0
+        if cnt == k:
+            answer += 1
+    print(f'#{tc} {answer}')

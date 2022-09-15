c80e39c6744329ab6044803be5f87391cbd4f4ed
JonghyeonC <109335419+JonghyeonC@users.noreply.github.com>
2022-09-15 13:55:50 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 155 ms, Memory: 58,512 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,28 +1,18 @@
 def inorder(idx):
-    if idx:
-        inorder(ch1[idx])
+    if idx <= N:
+        inorder(2 * idx)
         ans.append(tree[idx])
-        inorder(ch2[idx])
+        inorder(2 * idx + 1)
 
 
 T = 10
 for tc in range(1, T+1):
     N = int(input())
-    ch1 = [0] * (N + 1)
-    ch2 = [0] * (N + 1)
     tree = [0] * (N + 1)
     for _ in range(N):
         num_list = input().split()
         idx = int(num_list[0])
-        if len(num_list) == 4:
-            tree[idx] = num_list[1]
-            ch1[idx] = int(num_list[2])
-            ch2[idx] = int(num_list[3])
-        elif len(num_list) == 3:
-            tree[idx] = num_list[1]
-            ch1[idx] = int(num_list[2])
-        elif len(num_list) == 2:
-            tree[idx] = num_list[1]
+        tree[idx] = num_list[1]
     ans = []
     inorder(1)
     answer = ''.join(map(str, ans))

dd334e960a4f23c83073c5943a6dec2d7010b7ef
JonghyeonC <109335419+JonghyeonC@users.noreply.github.com>
2022-09-26 13:33:43 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 3,996 ms, Memory: 69,104 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -1,15 +1,14 @@
-def dfs(i, j, result):
+def dfs(i, j, result, c):
     global cnt
-    if len(result) == 7:
-        if result not in answer:
-            answer.append(result)
-            return
+    if c == 7:
+        answer.append(result)
+        return
     else:
         for k in range(4):
             ni = i + di[k]
             nj = j + dj[k]
             if 0 <= ni < N and 0 <= nj < N:
-                dfs(ni, nj, result + str(arr[ni][nj]))
+                dfs(ni, nj, result + arr[ni][nj], c + 1)
 
 
 di = [1, 0, -1, 0]
@@ -17,10 +16,10 @@ dj = [0, 1, 0, -1]
 T = int(input())
 for tc in range(1, T + 1):
     N = 4
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    arr = [input().split() for _ in range(N)]
     cnt = 0
     answer = []
     for i in range(N):
         for j in range(N):
-            dfs(i, j, str(arr[i][j]))
-    print(f'#{tc} {len(answer)}')
+            dfs(i, j, '', 0)
+    print(f'#{tc} {len(set(answer))}')

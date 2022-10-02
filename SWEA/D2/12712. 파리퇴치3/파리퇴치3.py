e5da8b50a84956e0fe9928fed204ec9c783703f8
JonghyeonC <109335419+JonghyeonC@users.noreply.github.com>
2022-10-02 22:38:06 +0900

    [D2] Title: 파리퇴치3, Time: 181 ms, Memory: 62,796 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
@@ -1,32 +1,27 @@
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
     di = [-1, 0, 1, 0]
     dj = [0, 1, 0, -1]
-    dx = [-1, -1, 1, 1]
-    dy = [-1, 1, 1, -1]
+    dii = [1, 1, -1, -1]
+    djj = [1, -1, -1, 1]
     ans = 0
     for i in range(N):
         for j in range(N):
-            total = arr[i][j]
+            total_1 = arr[i][j]
+            total_2 = arr[i][j]
             for k in range(4):
-                for l in range(1, M):
-                    ni = i + di[k] * l
-                    nj = j + dj[k] * l
+                for c in range(1, M):
+                    ni = i + di[k] * c
+                    nj = j + dj[k] * c
+                    nii = i + dii[k] * c
+                    njj = j + djj[k] * c
                     if 0 <= ni < N and 0 <= nj < N:
-                        total += arr[ni][nj]
-            if total > ans:
-                ans = total
-            total = arr[i][j]
-            for k in range(4):
-                for l in range(1, M):
-                    ni = i + dx[k] * l
-                    nj = j + dy[k] * l
-                    if 0 <= ni < N and 0 <= nj < N:
-                        total += arr[ni][nj]
-            if total > ans:
-                ans = total
-
+                        total_1 += arr[ni][nj]
+                    if 0 <= nii < N and 0 <= njj < N:
+                        total_2 += arr[nii][njj]
+            if ans < max(total_1, total_2):
+                ans = max(total_1, total_2)
     print(f'#{tc} {ans}')

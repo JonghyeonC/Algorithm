ce689a355f1196c0fbe7473d0d46514fdd948002
JonghyeonC <109335419+JonghyeonC@users.noreply.github.com>
2022-10-03 19:54:04 +0900

    [Silver II] Title: 부분수열의 합, Time: 560 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,18 +1,20 @@
-def dfs(r, k, s, total):
-    global cnt
-    if k == r:
+def f(k, total):
+    global ans
+    if k == N:
         if total == S:
-            cnt += 1
+            ans += 1
     else:
-        for i in range(s, N - r + 1 + k):
-            result[k] = arr[i]
-            dfs(r, k + 1, i + 1, total + arr[i])
+        result[k] = 0
+        f(k + 1, total)
+        result[k] = 1
+        f(k + 1, total + arr[k])
 
 
 N, S = map(int, input().split())
 arr = list(map(int, input().split()))
-cnt = 0
-for i in range(1, N+1):
-    result = [0] * i
-    dfs(i, 0, 0, 0)
-print(cnt)
+result = [0] * N
+ans = 0
+f(0, 0)
+if S == 0:
+    ans -= 1
+print(ans)

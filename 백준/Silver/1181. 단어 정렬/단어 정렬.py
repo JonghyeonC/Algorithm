04c4861b6d85334cacddce1ac3e8b92ba96aa823
JonghyeonC <109335419+JonghyeonC@users.noreply.github.com>
2022-12-26 23:50:12 +0900

    [Silver V] Title: 단어 정렬, Time: 760 ms, Memory: 35272 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,12 @@
+N = int(input())
+word_list = []
+for _ in range(N):
+    word_list.append(input())
+
+word_list = set(word_list)
+word_list = list(word_list)
+
+word_list.sort(key=lambda x: (len(x), x))
+
+for word in word_list:
+    print(word)

54d0c81f5ae45370cdbcd4d0f5b2ea24df1592e6
JonghyeonC <109335419+JonghyeonC@users.noreply.github.com>
2023-01-12 01:04:26 +0900

    [Silver II] Title: 파닭파닭, Time: 1144 ms, Memory: 189768 KB -BaekjoonHub

---
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.py
@@ -0,0 +1,23 @@
+S, C = map(int, input().split())
+
+sp = []
+for _ in range(S):
+    sp.append(int(input()))
+
+start = 1
+end = max(sp)
+ans = 0
+while start <= end:
+    cnt = 0
+    mid = (start + end) // 2
+
+    for num in sp:
+        cnt += num // mid
+
+    if cnt >= C:
+        start = mid + 1
+        ans = max(mid, ans)
+    else:
+        end = mid - 1
+
+print(sum(sp) - C * ans)

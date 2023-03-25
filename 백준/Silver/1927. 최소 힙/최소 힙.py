aaf3b936f1729eb34ddcfeef8a5dbd1176e0e616
JonghyeonC <109335419+JonghyeonC@users.noreply.github.com>
2023-03-25 22:54:15 +0900

    [Silver II] Title: 최소 힙, Time: 120 ms, Memory: 37180 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,14 @@
+import heapq, sys
+
+
+N = int(sys.stdin.readline())
+arr = []
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        if arr:
+            print(heapq.heappop(arr))
+        else:
+            print(0)
+    else:
+        heapq.heappush(arr, num)

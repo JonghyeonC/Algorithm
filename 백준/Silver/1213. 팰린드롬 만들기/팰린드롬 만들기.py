7878c0dbb40ff81c0619635996b2fd418a622da7
JongHyeon Choi <109335419+JonghyeonC@users.noreply.github.com>
2023-04-19 00:07:15 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,17 +1,28 @@
-ATOZ = input()
-flag = 0
-front = mid = back = ''
+text = input()
+# 알파벳의 갯수를 센다
+alpha_cnt = {}
+for i in text:
+    if i in alpha_cnt:
+        alpha_cnt[i] += 1
+    else:
+        alpha_cnt[i] = 1
 
-for i in range(65,91):
-    if chr(i) in ATOZ:
-        cnt = ATOZ.count(chr(i))
-        if cnt%2 == 1:
-            if mid != '': 
-                flag = 1
-                break
-            mid = chr(i)
-        
-        front = front + chr(i)*(cnt//2)
-        back = back + chr(i)*(cnt//2)
-            
-print( "I'm Sorry Hansoo" if flag else front + mid + back[::-1]) 
+alpha = []
+for temp in alpha_cnt:
+    alpha.append((temp, alpha_cnt[temp]))
+
+alpha.sort()
+
+odd_cnt = 0
+center = ''
+Palindrome = ''
+for temp in alpha:
+    if temp[1] % 2 != 0:
+        odd_cnt += 1
+        center += temp[0]
+    Palindrome += temp[0] * (temp[1] // 2)
+
+if odd_cnt > 1:
+    print("I'm Sorry Hansoo")
+else:
+    print(Palindrome + center + Palindrome[::-1])

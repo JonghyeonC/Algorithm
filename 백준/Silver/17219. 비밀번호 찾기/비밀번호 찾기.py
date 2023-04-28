821ae198ac2d1a31a44aa853604a266e677302de
JongHyeon Choi <109335419+JonghyeonC@users.noreply.github.com>
2023-04-28 22:29:12 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 232 ms, Memory: 49344 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,8 +1,11 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 pwd = {}
 for _ in range(N):
     a, b = input().split()
     pwd[a] = b
 for _ in range(M):
-    site = input()
+    site = input().rstrip()
     print(pwd[site])

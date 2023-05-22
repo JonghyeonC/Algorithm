c51a349c9f4b6f02f92a06684db5c66450b0bea6
JongHyeon Choi <109335419+JonghyeonC@users.noreply.github.com>
2023-05-22 22:21:56 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+N, K = map(int, input().rstrip().split())
+print(factorial(N) // (factorial(K) * factorial(N - K)))

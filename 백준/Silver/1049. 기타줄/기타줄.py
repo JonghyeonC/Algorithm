a79a2da931fa407907ab081c2f95fe4e629df83e
JongHyeon Choi <109335419+JonghyeonC@users.noreply.github.com>
2023-06-10 23:59:06 +0900

    [Silver IV] Title: 기타줄, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.py
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -1,4 +1,5 @@
-import math
+import math, sys
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 bun = 1001
@@ -9,10 +10,10 @@ for _ in range(M):
     bun = min(bun, a)
     per = min(per, b)
 
-if bun > per * 6:
+if bun > per * 6:   # 낱개가 무조건 더 저렴
     ans += N * per
-elif bun < (N % 6) * per:
+elif bun < (N % 6) * per:   # 번들이 더 저렴
     ans += bun * math.ceil(N / 6)
-else:
+else:   # 합쳐서 구매할 때
     ans += bun * (N // 6) + per * (N % 6)
 print(ans)

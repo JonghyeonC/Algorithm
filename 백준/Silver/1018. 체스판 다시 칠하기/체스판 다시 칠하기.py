1cf6bbda9c5ec17708b9c8f0f4b79f09b617598a
JongHyeon Choi <109335419+JonghyeonC@users.noreply.github.com>
2023-06-13 22:44:59 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+
+N, M = map(int, input().rstrip().split())
+board = [list(input().rstrip()) for _ in range(N)]
+min_cnt = 32
+for i in range(N - 7):
+    for j in range(M - 7):
+        tmp = 0
+        for x in range(i, i + 8):
+            for y in range(j, j + 8):
+                if (x + y) % 2 == 0:
+                    if board[x][y] != 'B':
+                        tmp += 1
+                else:
+                    if board[x][y] != 'W':
+                        tmp += 1
+        min_cnt = min(min_cnt, min(tmp, 64 - tmp))
+
+print(min_cnt)
